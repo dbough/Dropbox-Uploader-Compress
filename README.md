97773b65574bb32b0a2527a77a8774202c7c3c32
Daniel Bough <daniel.bough@gmail.com>
2013-04-02 06:22:29 -0400

    Added stuff.

--- README.md
+++ README.md
@@ -1,25 +1,34 @@
 THIS SOFTWARE IS IN DEVELOPMENT
-=
+===============================
 This software is in development and is not yet ready for use. 
 
-Dropbox-Uploader-Compress Author
--
+Dropbox Uploader Compress Author
+--------------------------------
 Author: Dan Bough  
 Email:  daniel.bough@gmail.com  
 Web:    http://www.danielbough.com  
 
-Dropbox-Uploader Author  
--
+Dropbox Uploader Author  
+-----------------------
 Author: Andrea Fabrizi  
 Email:  andrea.fabrizi@gmail.com  
 Web:    http://www.andreafabrizi.it  
 Readme:  RM.ORIG
 
 Introduction
--
-Dropbox-Uploader-Compress is a fork of Andrea Fabrizi's Dropbox-Uploader (https://github.com/andreafabrizi/Dropbox-Uploader).  It includes a Perl wrapper that adds extra functionality (folder compression, and target file/folder custom names for example.)  It does not change (and has no intentions of changing) Dropbox-Uploader.
+------------
+Dropbox Uploader Compress is a fork of Andrea Fabrizi's Dropbox-Uploader (https://github.com/andreafabrizi/Dropbox-Uploader).  It includes a Perl wrapper that adds extra functionality (folder compression, and target file/folder custom names for example.)  It does not change (and has no intentions of changing) Dropbox-Uploader.
 
 Requirements
--
-- GetOpt::Long (http://perldoc.perl.org/Getopt/Long.html).
+------------
+- Perl v5
+
+Instructions
+------------
+
+Changelog
+---------
+v0.1 - Unstable release.  Active development.
+
+
  
--- db_uploader_wrapper.pl
+++ db_uploader_wrapper.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl -w
+#!/usr/bin/perl
 
 # Dropbox Uploader Compress
 #
@@ -22,43 +22,43 @@
 # Debug stuff.
 
 use Data::Dumper;
+use warnings;
 
 #####
 
 use strict;
-use Getopt::Long;
+use Getopt::Long qw(:config no_auto_abbrev);
 use POSIX qw(strftime);
 
 # Configurable variables
-my $backupSourceFile; # Folder or file to back up.  Do not use FULL names.
-my $backupSourcePath; # Path that $backupSourceFile lives under.
-my $buPrefix = "default"; # Backed up file/folder filename prefix.
-my $buTargetFolder; # Folder on dropbox to back up to.
-my $buStagingFolder = "/tmp/"; # Local folder to stage your backup.  Used as a container to compress the backup.
-my $dbuScriptName = "dropbox_uploader.sh"; # Name of Dropbox_Uploader script.
-my $dbuScriptPath; # Path where Dropbox_Uploader lives.
-my $tar = "/bin/tar"; # Path to the tar exe.
-my $rm = "/bin/rm"; # Path to the rm exe.
-my $compress; # Compress files / folders before uploading?
-my $help; # Display help?
+my $backupSourceFile; 						# Folder or file to back up.  Do not use FULL names.
+my $backupSourcePath; 						# Path that $backupSourceFile lives under.
+my $buPrefix = "default"; 					# Backed up file/folder filename prefix.
+my $buTargetFolder; 						# Folder on dropbox to back up to.
+my $buStagingFolder = "/tmp/"; 				# Local folder to stage your backup.  Used as a container to compress the backup.
+my $dbuScriptName = "dropbox_uploader.sh"; 	# Name of Dropbox_Uploader script.
+my $dbuScriptPath; 							# Path where Dropbox_Uploader lives.
+my $tar = "/bin/tar"; 						# Path to the tar exe.
+my $rm = "/bin/rm"; 						# Path to the rm exe.
+my $compress; 								# Compress files / folders before uploading?
+my $help; 									# Display help?
 
 GetOptions (
-    "bu_source_file=s"   => \$backupSourceFile, # required
-    "bu_source_path=s"   => \$backupSourcePath, # required
-    "bu_prefix=s"   => \$buPrefix, # optional
-    "bu_target_folder=s"   => \$buTargetFolder, # required
-    "bu_staging_folder=s"   => \$buStagingFolder, # optional
-    "script_name=s"   => \$dbuScriptName, # optional
-    "script_path=s"   => \$dbuScriptPath, # required
-    "tar=s"   => \$tar, # optional
-    "rm=s"   => \$rm, # optional
-    "compress"  => \$compress, # optional
-    "help" => \$help
+    "bu_source_file=s"   => \$backupSourceFile, 	# required
+    "bu_source_path=s"   => \$backupSourcePath, 	# required
+    "bu_prefix=s"   => \$buPrefix,					# optional
+    "bu_target_folder=s"   => \$buTargetFolder, 	# required
+    "bu_staging_folder=s"   => \$buStagingFolder, 	# optional
+    "script_name=s"   => \$dbuScriptName, 			# optional
+    "script_path=s"   => \$dbuScriptPath, 			# required
+    "tar=s"   => \$tar, 							# optional
+    "rm=s"   => \$rm, 								# optional
+    "compress"  => \$compress, 						# optional
+    "help" => \$help								# optional
 ); 
 
 # Verify our required flags are set.  If not, send instructions to the screen.
 if ( !$backupSourceFile || !$backupSourcePath || !$buTargetFolder || !$dbuScriptPath || $help ) { readme(); }
-
 
 # Other variables.
 my $time = strftime "%Y-%m-%d_%H-%M-%S", localtime; # Formatted time (to use in the backed up file/folder name).
@@ -66,8 +66,7 @@ my $filename = $buPrefix . "_" . "backup_" . $time . ".tgz";
 my $backupTarget =  $buTargetFolder . $filename;
 my $dbuExe = $dbuScriptPath . $dbuScriptName;
 
-
-# All of our work is done here.  Compress, then backup to dropbox, then remove the local compressed file.
+# All of our work is done here.  Compress (if flag is set), then backup to dropbox, then remove the local compressed file (if flag is set).
 if ( $compress ) {
  system($tar . " -czf " . $buStagingFolder . $filename . " " . $backupSourcePath . $backupSourceFile);
 }
@@ -77,8 +76,10 @@ if ( $compress ) {
 }
 
 sub readme{
+	print "Dropbox Uploader Compress v0.1\n";
+	print "Dan Bough - daniel.bough\@gmail.com\n\n";
 	print "Usage:  ./db_uploader_wrapper.pl [OPTIONS]\n";
-    print "Example:  ./db_uploader_wrapper.pl --bu_source_file='bigfile.txt' --bu_source_path='/home/yourname/' --backup_target_folder='Backups' --script_path='/usr/sbin/' --compress\n\n";
+    print "Example:  ./db_uploader_wrapper.pl --bu_source_file='bigfile.txt' --bu_source_path='/home/yourname/' --bu_target_folder='Backups' --script_path='/usr/sbin/' --compress\n\n";
     print "--bu_source_file     Required:  Folder of file to back up.  Do NOT use full paths.\n";
     print "--bu_source_path     Required:  Path to --bu_source_file.\n";
     print "--bu_prefix          Optional:  Backed up file/folder filename prefix. (Defaults to 'default').\n";
@@ -88,7 +89,7 @@ sub readme{
     print "--script_path        Required:  Path where dropbox_uploader.sh lives\n";
     print "--tar                Optional:  Path to tar (defaults to /bin/tar).\n";
     print "--rm                 Optional:  Path to rm (defaults to /bin/rm).\n";
-    print "--Compress           Optional:  Compress file or folder (tar and gzip).\n";       
+    print "--compress           Optional:  Compress file or folder (tar and gzip).\n";       
     print "--help               Optional:  Display this message.\n\n";
 	exit;
 }
