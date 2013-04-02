a92979aa4cbf05a7f68e2111707e5cd794436cda
Daniel Bough <daniel.bough@gmail.com>
2013-04-01 21:10:21 -0400

    Added LICENSE file, added more to wrapper

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
-Author
+THIS SOFTWARE IS IN DEVELOPMENT
+=
+This software is in development and is not yet ready for use. 
+
+Reloaded Author
 -
 Author: Dan Bough  
 Email:  daniel.bough@gmail.com  
 Web:    http://www.danielbough.com  
 
-Original Author Details  
+Original Author  
 -
 Author: Andrea Fabrizi  
 Email:  andrea.fabrizi@gmail.com  
@@ -13,12 +17,9 @@ Readme:  RM.ORIG
 
 Introduction
 -
-Dropbox-Uploader-Reload is a fork of Andrea Fabrizi's Dropbox-Uploader (https://github.com/andreafabrizi/Dropbox-Uploader).  It includes a Perl wrapper that adds extra functionality (folder compression, and target file/folder custom names for example.)
+Dropbox-Uploader-Reloaded is a fork of Andrea Fabrizi's Dropbox-Uploader (https://github.com/andreafabrizi/Dropbox-Uploader).  It includes a Perl wrapper that adds extra functionality (folder compression, and target file/folder custom names for example.)
 
 Requirements
 -
 - GetOpt::Long (http://perldoc.perl.org/Getopt/Long.html).
-
-THIS SOFTWARE IS IN DEVELOPMENT
-=
-This software is in development and is not yet ready for use.  
+ 
--- db_uploader_wrapper.pl
+++ db_uploader_wrapper.pl
@@ -1,4 +1,30 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -w
+
+# Dropbox Uploader Reloaded
+#
+# Copyright (C) 2010-2013 Dan Bough <daniel.bough@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+
+# Debug stuff.
+
+use Data::Dumper;
+
+#####
+
 use strict;
 use Getopt::Long;
 use POSIX qw(strftime);
@@ -14,16 +40,25 @@ my $dbuScriptPath; # Path where Dropbox_Uploader lives.
 my $tar = "/bin/tar"; # Path to the tar exe.
 my $rm = "/bin/rm"; # Path to the rm exe.
 my $compress; # Compress files / folders before uploading?
+my $help; # Display help?
 
-my $options = GetOptions (
-	# "length=i" => \$length,    # numeric
-    # "file=s"   => \$data,      # string
-	"compress"  => \$compress	   # flag
-);  
+GetOptions (
+    "bu_source_file=s"   => \$backupSourceFile, # required
+    "bu_source_path=s"   => \$backupSourcePath, # required
+    "bu_prefix=s"   => \$buPrefix, # optional
+    "bu_target_folder=s"   => \$buTargetFolder, # required
+    "bu_staging_folder=s"   => \$buStagingFolder, # optional
+    "script_name=s"   => \$dbuScriptName, # optional
+    "script_path=s"   => \$dbuScriptPath, # required
+    "tar=s"   => \$tar, # optional
+    "rm=s"   => \$rm, # optional
+    "compress"  => \$compress, # optional
+    "help" => \$help
+); 
 
-if( !$options ) {
-	readme();
-}
+# Verify our required flags are set.  If not, send instructions to the screen.
+if ( !$backupSourceFile || !$backupSourcePath || !$buTargetFolder || !$dbuScriptPath || $help ) { readme(); }
+
 
 # Other variables.
 my $time = strftime "%Y-%m-%d_%H-%M-%S", localtime; # Formatted time (to use in the backed up file/folder name).
@@ -31,14 +66,29 @@ my $filename = $buPrefix . "_" . "backup_" . $time . ".tgz";
 my $backupTarget =  $buTargetFolder . $filename;
 my $dbuExe = $dbuScriptPath . $dbuScriptName;
 
-system($tar . " -cvzf " . $buStagingFolder . $filename . " " . $backupSourcePath . $backupSourceFile);
-system($dbuScriptPath . $dbuScriptName . " upload " . $buStagingFolder . $filename . " " . $backupTarget);
-system($rm . " " . $buStagingFolder . $filename);
 
-sub readme{
-	print "Hi\n";
-	exit;
+# All of our work is done here.  Compress, then backup to dropbox, then remove the local compressed file.
+if ( $compress ) {
+ system($tar . " -czf " . $buStagingFolder . $filename . " " . $backupSourcePath . $backupSourceFile);
+}
+system($dbuScriptPath . $dbuScriptName . " upload " . $buStagingFolder . $filename . " " . $backupTarget);
+if ( $compress ) {
+    system($rm . " " . $buStagingFolder . $filename);
 }
 
-
-
+sub readme{
+	print "Usage:  ./db_uploader_wrapper.pl [OPTIONS]\n";
+    print "Example:  ./db_uploader_wrapper.pl --bu_source_file='bigfile.txt' --bu_source_path='/home/yourname/' --backup_target_folder='Backups' --script_path='/usr/sbin/' --compress\n\n";
+    print "--bu_source_file     Required:  Folder of file to back up.  Do NOT use full paths.\n";
+    print "--bu_source_path     Required:  Path to --bu_source_file.\n";
+    print "--bu_prefix          Optional:  Backed up file/folder filename prefix. (Defaults to 'default').\n";
+    print "--bu_target_folder   Required:  Folder on dropbox to back up to.\n";
+    print "--bu_staging_folder  Optional:  Folder to stage backkup file / folder (it gets compressed here & then gets removed after it's uploaded.  Defaults to /tmp/.)\n";
+    print "--script_name        Optional:  Name of Dropbox Uploader shell script (defaults to dropbox_uploader.sh).\n";
+    print "--script_path        Required:  Path where dropbox_uploader.sh lives\n";
+    print "--tar                Optional:  Path to tar (defaults to /bin/tar).\n";
+    print "--rm                 Optional:  Path to rm (defaults to /bin/rm).\n";
+    print "--Compress           Optional:  Compress file or folder (tar and gzip).\n";       
+    print "--help               Optional:  Display this message.\n\n";
+	exit;
+}
